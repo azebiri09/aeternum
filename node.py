06a2524ae15b182d03136b25135daa497c181148
azebiri09 <azebiri09@gmail.com>
2026-06-13 15:06:04 +0100

    Add peer discovery

--- node.py
+++ node.py
@@ -1,17 +1,29 @@
 
 import socket
+import threading
+import json
 
-def start_node(host, port):
+peers = []
+
+def handle_peer(conn):
+    data = conn.recv(1024).decode()
+    if data == "GET_PEERS":
+        conn.send(json.dumps(peers).encode())
+    else:
+        if data not in peers:
+            peers.append(data)
+            print(f"New peer: {data}")
+        conn.send(b"OK")
+    conn.close()
+
+def start_node(port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
+    server.bind(("0.0.0.0", port))
     server.listen(5)
-    print(f"Aeternum node running on {host}:{port}")
+    print(f"Aeternum node running on port {port}")
+    print(f"Peers: {peers}")
     while True:
         conn, addr = server.accept()
-        print(f"Connected by {addr}")
-        data = conn.recv(1024)
-        print(f"Received: {data.decode()}")
-        conn.send(b"Aeternum node received your message")
-        conn.close()
+        threading.Thread(target=handle_peer, args=(conn,)).start()
 
-start_node("0.0.0.0", 9000)
+start_node(9000)
